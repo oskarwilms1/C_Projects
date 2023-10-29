4e58f1f4a81c27ecfd9b4b3309779e5f6b25185a
oskarwilms1 <o.wilms.062@studms.ug.edu.pl>
2023-10-29 10:37:50 +0100

    Zadania od 1-6

---
+++ Lab3/Zadanie3/Wiekszy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a, b;
+    printf("Użytkowniku! Podaj 2 liczby całkowite oddzielone spacją!\n");
+    scanf("%d %d", &a, &b);
+    if(a>b)
+    {
+        printf("Większa liczba to: %d\n", a);
+    }
+    if(b>a)
+    {
+        printf("Większa liczba to: %d\n", b);
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
