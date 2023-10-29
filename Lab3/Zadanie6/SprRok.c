06341b04ec0a44e0ba49dca0a9c6ff13b70e1ff3
oskarwilms1 <o.wilms.062@studms.ug.edu.pl>
2023-10-29 11:38:03 +0100

    Zadanie 7

--- Lab3/Zadanie6/SprRok.c
+++ Lab3/Zadanie6/SprRok.c
@@ -3,9 +3,20 @@
 
 int main()
 {
+    int a;
+    printf("Podaj rok: \n");
+    scanf("%d", &a);
+    int p = a%4;
+    if(p!=0)
+    {
+        printf("Rok %d nie jest rokiem przestępnym\n", a);
+    }
+    if(p==0)
+    {
+        printf("Rok %d jest rokiem przestępnym\n", a);
+    }
 
-    
-
+    return 0;
 
 
 
