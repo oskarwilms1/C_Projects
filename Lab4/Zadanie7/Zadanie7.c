4d6118ff92e210d00d5fc0b9ceadb5fa15f25b93
oskarwilms1 <o.wilms.062@studms.ug.edu.pl>
2023-11-19 11:45:08 +0100

    Lab5

---
+++ Lab4/Zadanie7/Zadanie7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n , x=2;
+    printf("Podaj liczbę:");
+    scanf("%d", &n);
+    while (x < n)
+    {
+        if (n % x = 0)
+        {
+            print("Liczba nie jest liczbą pierwszą");
+            x == n
+        }
+        else:
+    }
+
+
+
+
+
+}
